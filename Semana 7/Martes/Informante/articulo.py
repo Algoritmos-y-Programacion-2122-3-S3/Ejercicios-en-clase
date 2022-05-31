712dd36394565506d47d29acb9cfb8e4b6eedb9d
Rommel <rommel.sanzonetti@gmail.com>
2022-05-31 08:23:06 -0400

    ✨ Clase Martes Semana 7 - herencia

---
+++ Semana 7/Martes/Informante/articulo.py
@@ -0,0 +1,9 @@
+class Articulo:
+    def __init__(self,titulo,resumen,cuerpo,imágenes,fecha_publicación,fecha_creación,redactor):
+        self.titulo  = titulo
+        self.resumen =resumen
+        self.cuerpo = cuerpo
+        self.imágenes =  imágenes
+        self.fecha_publicación = fecha_publicación
+        self.fecha_creación = fecha_creación
+        self.redactor = redactor
